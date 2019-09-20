cc3e698717f12caf0fc227175cdba2c13c864367
resuryasin <672016198@student.uksw.edu>
2019-09-21 04:44:09 +0700

    Process 98% complete

--- org_eval/modules/process.py
+++ org_eval/modules/process.py
@@ -1,68 +1,116 @@
-def make_sheet_recap(config):
-    sheet = config.load_sheet(config.config['resources_path']+'/format/process.xlsx')
-    active_sheet_idx = 0
-    miss_nim = []
-    import os.path
-    for cat in config.data:
-        for nim in config.data[cat]:
-            if not os.path.exists(config.config['resources_path']+"/input/"+nim+".xlsx"):
-                miss_nim.append(nim)
-            sheet.copy_worksheet(sheet.active)
-            active_sheet_idx += 1
-            sheet.active = active_sheet_idx
-            sheet.active.title = nim
-            sheet.active["B1"].value = config.data[cat][nim]['name']
-            sheet.active["B2"].value = nim
-            sheet.active["B3"].value = config.data[cat][nim]['position']
-            sheet.active = 0
-    return sheet, miss_nim
+class Process():
+    def __init__(self, excel_name='data'):
+        self.__all = self.importExcel(excel_name, 0).dropna()
+        self.__popRes, self.__popFail = self.populating()
+        self.__generateExcelProcess()
+        self.fillDimension()
 
-def main_process(config):
-    miss_nim = []
-    try:
-        import os.path
-        file_recap, miss_nim = make_sheet_recap(config)
-        if len(miss_nim) > 0:
-            print(f'There is {len(miss_nim)} missing file(s): \n{miss_nim}')
-            cont = input('Continue? (Yes/No) default: No\nAnswer: ').lower() or 'No'
-            if cont in ('yes','y','ya'):
-                from tqdm import tqdm
-                tqdm.write("Processing Files...")
-                for cat in tqdm(config.data,ascii=True,desc="Result", position=1):
-                    for nim in tqdm(config.data[cat],ascii=True,desc=cat, position=0):
-                        try:
-                            file_ref = config.load_sheet(config.config['resources_path']+"/input/"+nim+".xlsx", 1)
-                        except:
-                            continue
-                        # if os.path.exists(config.config['resources_path']+"/input/"+nim+".xlsx"):
-                        #     file_ref = config.load_sheet(config.config['resources_path']+"/input/"+nim+".xlsx", 1)
-                        # else:
-                        #     continue
-                        for row in tqdm(range(5,file_ref.active.max_row,7),desc=nim,ascii=True,position=2):
-                            if file_ref.active["C"+str(row)].value != nim and file_ref.active["C"+str(row)].value != None:
-                                try:
-                                    file_recap.active = file_recap[file_ref.active["C"+str(row)].value]
-                                except:
-                                    continue
-                                init = 3
-                                while file_recap.active["E"+str(init)].value != None:
-                                    init+=1
-                                file_recap.active["E"+str(init)].value = config.data[cat][nim]['name']
-                                file_recap.active["F"+str(init)].value = nim
-                                file_recap.active["G"+str(init)].value = config.data[cat][nim]['position']
-                                file_recap.active["H"+str(init)].value = file_ref.active["F"+str(row+5)].value
-                                file_recap.active["I"+str(init)].value = file_ref.active["G"+str(row+5)].value
-                                file_recap.active["J"+str(init)].value = file_ref.active["H"+str(row+5)].value
-                                file_recap.active["K"+str(init)].value = file_ref.active["K"+str(row+5)].value
-                                file_recap.active["L"+str(init)].value = file_ref.active["N"+str(row+5)].value
-                                file_recap.active["M"+str(init)].value = file_ref.active["S"+str(row+5)].value
-                                file_recap.active["N"+str(init)].value = file_ref.active["W"+str(row+5)].value
-                                file_recap.active["O"+str(init)].value = file_ref.active["AA"+str(row+5)].value
-                file_recap.active = 0
-                file_recap[file_recap.active.title].sheet_state = "hidden"
-                file_recap.save(config.config['resources_path']+'/output/'+config.folder['dirname'][1]+"/Evaluation Summaries.xlsx")
-    except Exception as e:
-        print(e)
-    finally:
-        input('\nHappy Evaluation\nBest Regard RSS\n')
-    return
+    @staticmethod
+    def importExcel(filename, sheetname, usecols=None, header=0, nrows=None):
+        import pandas as pd
+        return pd.read_excel('resources/{}.xlsx'.format(filename), header=header, sheet_name=sheetname, usecols=usecols,
+                             nrows=nrows)
+
+    def populating(self):
+        import os
+        file = (os.listdir('resources\input'))
+        result = []
+        resFail = []
+        for i in file:
+            result.append(self.generateDim(i[:9])) if self.generateDim(i[:9]) else resFail.append(i)
+        return result, resFail
+
+    def generateDim(self, file):
+        profile = []
+        for i in range(len(self.__all.NIM)):
+            if file == str(self.__all.NIM[i])[:9]:
+                profile.append(self.__all.Nama[i])
+                profile.append(str(self.__all.NIM[i])[:9])
+                profile.append(self.__all.Jabatan[i])
+        from numpy import average as avg
+        from openpyxl import load_workbook
+        result = []
+        try:
+            workbook = load_workbook('resources/input/{file}.xlsx'.format(file=file)).sheetnames
+            for sheet in workbook:
+                nim = self.importExcel('input/{file}'.format(file=file), sheet, "B,G,L,Q", None, 1)
+                data = self.importExcel('input/{file}'.format(file=file), sheet, "B,G,L,Q", 1).fillna(0)
+                dimension = [data.iloc[0][1],  # Dimension 1
+                             data.iloc[1][1],  # Dimension 2
+                             avg([avg([data.iloc[2][1], data.iloc[3][1]]), data.iloc[4][1]]),  # Dimension 3
+                             avg([avg([data.iloc[5][1], data.iloc[6][1]]), data.iloc[7][1]]),  # Dimension 4
+                             avg([avg([data.iloc[8][1], data.iloc[9][1]]),
+                                  avg([data.iloc[10][1], data.iloc[11][1], data.iloc[12][1]])]),  # Dimension 5
+                             avg([avg([data.iloc[13][1], data.iloc[14][1], data.iloc[15][1]]), data.iloc[16][1]]),
+                             # Dimension 6
+                             avg([data.iloc[17][1], avg([data.iloc[18][1], data.iloc[19][1], data.iloc[20][1]])]),
+                             # Dimension 7
+                             avg([avg([data.iloc[21][1], data.iloc[22][1], data.iloc[23][1]]),
+                                  avg([data.iloc[24][1], data.iloc[25][1], data.iloc[26][1]]),
+                                  avg([data.iloc[27][1], data.iloc[28][1]]),
+                                  avg([data.iloc[29][1], data.iloc[30][1], data.iloc[31][1]])]),  # Dimension 8
+                             profile  # Add Profile
+                             ]
+                result.append({nim.iloc[0][6]: dimension})
+            return {file: result}
+        except Exception as e:
+            print(e)
+            return None
+
+    def fillDimension(self):
+        from openpyxl import load_workbook
+        from openpyxl.styles import Protection
+        from openpyxl.workbook.protection import WorkbookProtection
+        workbook = load_workbook('resources/output/process/Summaries.xlsx')
+        for pop in self.__popRes:
+            for _ in pop:
+                for index in pop[_]:
+                    try:
+                        for k, v in index.items():
+                            init = 3
+                            workbook.active = workbook[str(k)]
+                            while workbook.active['E{}'.format(str(init))].value != None:
+                                init += 1
+                            workbook.active.protection.sheet = True
+                            workbook.active.protection.password = 'sweet'
+                            for x in range(3, 100):
+                                workbook.active['H' + str(x)].protection = Protection(locked=False)
+                                workbook.active['I' + str(x)].protection = Protection(locked=False)
+                                workbook.active['J' + str(x)].protection = Protection(locked=False)
+                                workbook.active['K' + str(x)].protection = Protection(locked=False)
+                                workbook.active['L' + str(x)].protection = Protection(locked=False)
+                                workbook.active['M' + str(x)].protection = Protection(locked=False)
+                                workbook.active['N' + str(x)].protection = Protection(locked=False)
+                                workbook.active['O' + str(x)].protection = Protection(locked=False)
+                            workbook.active['E{}'.format(str(init))].value = v[8][0]
+                            workbook.active['F{}'.format(str(init))].value = v[8][1]
+                            workbook.active['G{}'.format(str(init))].value = v[8][2]
+                            workbook.active['H{}'.format(str(init))].value = v[0]
+                            workbook.active['I{}'.format(str(init))].value = v[1]
+                            workbook.active['J{}'.format(str(init))].value = v[2]
+                            workbook.active['K{}'.format(str(init))].value = v[3]
+                            workbook.active['L{}'.format(str(init))].value = v[4]
+                            workbook.active['M{}'.format(str(init))].value = v[5]
+                            workbook.active['N{}'.format(str(init))].value = v[6]
+                            workbook.active['O{}'.format(str(init))].value = v[7]
+                    except Exception as e:
+                        print(e)
+        workbook.security = WorkbookProtection(workbookPassword='sweet', lockWindows=True, lockStructure=True)
+        workbook.save("resources/output/process/Summaries.xlsx")
+        workbook.close()
+
+    def __generateExcelProcess(self):
+        from openpyxl import load_workbook
+        workbook = load_workbook('resources/format/process.xlsx')
+        for i in range(len(self.__all)):
+            workbook.active['B1'].value = self.__all.Nama.values[i]
+            workbook.active['B2'].value = self.__all.NIM.values[i]
+            workbook.active['B3'].value = self.__all.Jabatan.values[i]
+            workbook.active['B4'].value = self.__all.Nama.values[i]
+            workbook.active['B5'].value = self.__all.Nama.values[i]
+            workbook.active.title = str(self.__all.NIM.values[i])[:9]
+            workbook.copy_worksheet(workbook.active)
+            workbook.active = len(workbook.sheetnames) - 1
+        workbook.remove(workbook.active)
+        workbook.save("resources/output/process/Summaries.xlsx")
+        workbook.close()
--- org_eval/test.py
+++ org_eval/test.py
@@ -1,59 +1,118 @@
 class Process():
     def __init__(self, excel_name='data'):
         self.__all = self.importExcel(excel_name, 0).dropna()
-        self.xxx()
-        self.__dictList = {}
-        # self.__generateExcelProcess()
+        self.__popRes, self.__popFail = self.populating()
+        self.__generateExcelProcess()
+        self.fillDimension()
+        # print(self.__popRes)
+        # print(self.__popFail)
 
     @staticmethod
-    def importExcel(filename, sheetname,usecols=None,header=0,nrows=None):
+    def importExcel(filename, sheetname, usecols=None, header=0, nrows=None):
         import pandas as pd
-        return pd.read_excel('resources/{}.xlsx'.format(filename), header = header, sheet_name = sheetname, usecols=usecols,nrows=nrows)
+        return pd.read_excel('resources/{}.xlsx'.format(filename), header=header, sheet_name=sheetname, usecols=usecols,
+                             nrows=nrows)
 
-    def generateDim(self):
+    def populating(self):
+        import os
+        file = (os.listdir('resources\input'))
+        result = []
+        resFail = []
+        for i in file:
+            result.append(self.generateDim(i[:9])) if self.generateDim(i[:9]) else resFail.append(i)
+        return result, resFail
+
+    def generateDim(self, file):
+        profile = []
+        for i in range(len(self.__all.NIM)):
+            if file == str(self.__all.NIM[i])[:9]:
+                profile.append(self.__all.Nama[i])
+                profile.append(str(self.__all.NIM[i])[:9])
+                profile.append(self.__all.Jabatan[i])
         from numpy import average as avg
-        nim = self.importExcel('input/462015039',0,"B,G,L,Q",None,1)
-        all = self.importExcel('input/462015039',0,"B,G,L,Q",1).fillna(0)
-        result = [all.iloc[0][1],
-        all.iloc[1][1],
-        avg([avg([all.iloc[2][1],all.iloc[3][1]]),all.iloc[4][1]]),
-        avg([avg([all.iloc[5][1],all.iloc[6][1]]),all.iloc[7][1]]),
-        avg([avg([all.iloc[8][1],all.iloc[9][1]]),avg([all.iloc[10][1],all.iloc[11][1],all.iloc[12][1]])]),
-        avg([avg([all.iloc[13][1],all.iloc[14][1],all.iloc[15][1]]),all.iloc[16][1]]),
-        avg([all.iloc[17][1],avg([all.iloc[18][1],all.iloc[19][1],all.iloc[20][1]])]),
-        avg([avg([all.iloc[21][1],all.iloc[22][1],all.iloc[23][1]]),avg([all.iloc[24][1],all.iloc[25][1],all.iloc[26][1]]),avg([all.iloc[27][1],all.iloc[28][1]]),avg([all.iloc[29][1],all.iloc[30][1],all.iloc[31][1]])])
-        ]
-        return {nim.iloc[0][6]:result}
-    
+        from openpyxl import load_workbook
+        result = []
+        try:
+            workbook = load_workbook('resources/input/{file}.xlsx'.format(file=file)).sheetnames
+            for sheet in workbook:
+                nim = self.importExcel('input/{file}'.format(file=file), sheet, "B,G,L,Q", None, 1)
+                data = self.importExcel('input/{file}'.format(file=file), sheet, "B,G,L,Q", 1).fillna(0)
+                dimension = [data.iloc[0][1],  # Dimension 1
+                             data.iloc[1][1],  # Dimension 2
+                             avg([avg([data.iloc[2][1], data.iloc[3][1]]), data.iloc[4][1]]),  # Dimension 3
+                             avg([avg([data.iloc[5][1], data.iloc[6][1]]), data.iloc[7][1]]),  # Dimension 4
+                             avg([avg([data.iloc[8][1], data.iloc[9][1]]),
+                                  avg([data.iloc[10][1], data.iloc[11][1], data.iloc[12][1]])]),  # Dimension 5
+                             avg([avg([data.iloc[13][1], data.iloc[14][1], data.iloc[15][1]]), data.iloc[16][1]]),
+                             # Dimension 6
+                             avg([data.iloc[17][1], avg([data.iloc[18][1], data.iloc[19][1], data.iloc[20][1]])]),
+                             # Dimension 7
+                             avg([avg([data.iloc[21][1], data.iloc[22][1], data.iloc[23][1]]),
+                                  avg([data.iloc[24][1], data.iloc[25][1], data.iloc[26][1]]),
+                                  avg([data.iloc[27][1], data.iloc[28][1]]),
+                                  avg([data.iloc[29][1], data.iloc[30][1], data.iloc[31][1]])]),  # Dimension 8
+                             profile  # Add Profile
+                             ]
+                result.append({nim.iloc[0][6]: dimension})
+            return {file: result}
+        except Exception as e:
+            print(e)
+            return None
 
-    def __generateExcelProcess(self):
+    def fillDimension(self):
         from openpyxl import load_workbook
         from openpyxl.styles import Protection
         from openpyxl.workbook.protection import WorkbookProtection
+        workbook = load_workbook('resources/output/process/Summaries.xlsx')
+        for pop in self.__popRes:
+            for _ in pop:
+                for index in pop[_]:
+                    try:
+                        for k, v in index.items():
+                            init = 3
+                            workbook.active = workbook[str(k)]
+                            while workbook.active['E{}'.format(str(init))].value != None:
+                                init += 1
+                            workbook.active.protection.sheet = True
+                            workbook.active.protection.password = 'sweet'
+                            for x in range(3, 100):
+                                workbook.active['H' + str(x)].protection = Protection(locked=False)
+                                workbook.active['I' + str(x)].protection = Protection(locked=False)
+                                workbook.active['J' + str(x)].protection = Protection(locked=False)
+                                workbook.active['K' + str(x)].protection = Protection(locked=False)
+                                workbook.active['L' + str(x)].protection = Protection(locked=False)
+                                workbook.active['M' + str(x)].protection = Protection(locked=False)
+                                workbook.active['N' + str(x)].protection = Protection(locked=False)
+                                workbook.active['O' + str(x)].protection = Protection(locked=False)
+                            workbook.active['E{}'.format(str(init))].value = v[8][0]
+                            workbook.active['F{}'.format(str(init))].value = v[8][1]
+                            workbook.active['G{}'.format(str(init))].value = v[8][2]
+                            workbook.active['H{}'.format(str(init))].value = v[0]
+                            workbook.active['I{}'.format(str(init))].value = v[1]
+                            workbook.active['J{}'.format(str(init))].value = v[2]
+                            workbook.active['K{}'.format(str(init))].value = v[3]
+                            workbook.active['L{}'.format(str(init))].value = v[4]
+                            workbook.active['M{}'.format(str(init))].value = v[5]
+                            workbook.active['N{}'.format(str(init))].value = v[6]
+                            workbook.active['O{}'.format(str(init))].value = v[7]
+                    except Exception as e:
+                        print(e)
+        workbook.security = WorkbookProtection(workbookPassword='sweet', lockWindows=True, lockStructure=True)
+        workbook.save("resources/output/process/Summaries.xlsx")
+        workbook.close()
+
+    def __generateExcelProcess(self):
+        from openpyxl import load_workbook
         workbook = load_workbook('resources/format/process.xlsx')
         for i in range(len(self.__all)):
-            workbook.active.protection.sheet = True
-            workbook.active.protection.password = 'sweet'
-            for x in range(3,100):
-                workbook.active['H'+str(x)].protection = Protection(locked=False)
-                workbook.active['I'+str(x)].protection = Protection(locked=False)
-                workbook.active['J'+str(x)].protection = Protection(locked=False)
-                workbook.active['K'+str(x)].protection = Protection(locked=False)
-                workbook.active['L'+str(x)].protection = Protection(locked=False)
-                workbook.active['M'+str(x)].protection = Protection(locked=False)
-                workbook.active['N'+str(x)].protection = Protection(locked=False)
-                workbook.active['O'+str(x)].protection = Protection(locked=False)
             workbook.active['B1'].value = self.__all.Nama.values[i]
             workbook.active['B2'].value = self.__all.NIM.values[i]
             workbook.active['B3'].value = self.__all.Jabatan.values[i]
             workbook.active['B4'].value = self.__all.Nama.values[i]
             workbook.active['B5'].value = self.__all.Nama.values[i]
-            workbook.active.title = str(self.__all.NIM.values[i])
+            workbook.active.title = str(self.__all.NIM.values[i])[:9]
             workbook.copy_worksheet(workbook.active)
-            workbook.active = len(workbook.sheetnames)-1
+            workbook.active = len(workbook.sheetnames) - 1
         workbook.remove(workbook.active)
-        workbook.security = WorkbookProtection(workbookPassword='sweet', lockWindows=True, lockStructure=True)
-        workbook.save("resources/output/process/Summaries Evaluation.xlsx")
+        workbook.save("resources/output/process/Summaries.xlsx")
         workbook.close()
-
-x = Process()
